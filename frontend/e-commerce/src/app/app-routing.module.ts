9682a0ebbf7d6e00143017e8ce7ed5fe8ed03076
Belgacem Zabbes <36985434+Belgacem753@users.noreply.github.com>
2020-07-28 20:19:43 +0100

    first

--- frontend/e-commerce/src/app/app-routing.module.ts
+++ frontend/e-commerce/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShowProductComponent } from './show-product/show-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'product', component: ShowProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
