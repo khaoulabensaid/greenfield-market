1f96673ebaa23670c68e78c714e7505d822edaff
mella-kais <mallah.kais@gmail.com>
2020-08-03 15:48:07 +0100

    finished sending register and need some cleaning

--- frontend/e-commerce/src/app/app-routing.module.ts
+++ frontend/e-commerce/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events/events.component'; 
+import { SpecialsComponent } from './specials/specials.component'; 
+import { LoginComponent } from './login/login.component'; 
+import { RegisterComponent } from './register/register.component'; 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+  {
+    path:'events',
+    component: EventsComponent
+  },
+  {
+    path:'special',
+    component: SpecialsComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/e-commerce/src/app/app.module.ts
+++ frontend/e-commerce/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AuthService } from './auth.service';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+import { RegisterComponent } from './register/register.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialsComponent } from './specials/specials.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegisterComponent,
+    EventsComponent,
+    SpecialsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule, 
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
